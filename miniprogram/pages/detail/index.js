c7110479319e1d5f0b0556659fcdeba31da34cf0
Luke Na <narukeu@outlook.com>
2023-05-06 20:38:10 +0800

    接口 初次调用

--- miniprogram/pages/detail/index.js
+++ miniprogram/pages/detail/index.js
@@ -1,66 +1,101 @@
 // pages/detail/detail.ts
+var http = require("../../utils/http.js");
+
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    pd_type: '',
+    dtinfo: {},
+    pd_name: '',
+    rel_year: '',
+    model: '',
+    pd_imgs:[]
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    var pd_type = options.pd_type
+    console.log("调试：接收的参数（产品类型）是 " + pd_type)
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad() {
+    wx.showLoading({
+      title: '正在加载',
+    })
 
-    },
+    http.cloudGet("/api/model-query/models/" + pd_type).then((res) => {
+        console.log(res.data.data);
+        let dtinfo = res.data.data
+        let {
+          pd_name,
+          rel_year,
+          model,
+          pd_imgs
+        } = dtinfo
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+        this.setData({
+            dtinfo,
+            pd_name,
+            rel_year,
+            model,
+            pd_imgs
+          }),
+          console.log(dtinfo)
+        wx.hideLoading()
+      },
+      //console.log(dtname)
+    )
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
 
-    },
+  onReady() {
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
+  },
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
+  },
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
+  },
 
-    }
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
 })
