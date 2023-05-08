cc00ce04befc27272d59fd6b6cf27171cf29b2bf
Luke Na <narukeu@outlook.com>
2023-05-08 15:35:03 +0800

    进一步优化代码

--- miniprogram/utils/youros.js
+++ miniprogram/utils/youros.js
@@ -1,18 +1,17 @@
 // Made By Luke Na
 // 2023.05.05
 
-const app = getApp();
-
 // 以数组的方式输出
-
 const youros = () => {
-    const nowbrand = wx.getSystemInfoSync().brand
-    const nowos = wx.getSystemInfoSync().system
-    const nowmodel = wx.getSystemInfoSync().model
-    const scrwidth = wx.getSystemInfoSync().screenWidth
-    const scrheight = wx.getSystemInfoSync().screenHeight
-    
-    return [nowbrand, nowos, nowmodel, scrwidth, scrheight]
+  const {
+    brand,
+    system,
+    model,
+    screenWidth,
+    screenHeight
+  } = wx.getSystemInfoSync();
+
+  return [brand, system, model, screenWidth, screenHeight];
 }
 
-module.exports = youros
+module.exports = youros;
