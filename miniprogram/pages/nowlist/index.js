02e2fbe20898a250514dd9756183b2bd3d7c9ae8
Luke Na <narukeu@outlook.com>
2023-05-08 16:51:39 +0800

    2023-05-08 16:51 Updated

--- miniprogram/pages/nowlist/index.js
+++ miniprogram/pages/nowlist/index.js
@@ -1,10 +1,23 @@
 Page({
   data: {
+    yrinfo:{},
     indexList: [],
     list: [{
       index: 'A',
       children: ['阿坝', '阿拉善', '阿里', '安康', '安庆', '鞍山', '安顺', '安阳', '澳门'],
-    } ]
+    }]
+  },
+
+  onload(options) {
+    http.cloudGet("/api/model-query/group").then((res) => {
+      console.log(res.data.data);
+      let yrinfo = res.data.data
+      this.setData({
+        yrinfo
+      })
+      console.log(yrinfo)
+      wx.hideLoading()
+    }, )
   },
 
   onReady() {
