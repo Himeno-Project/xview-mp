cdfd94e55c9a5cf0f9b2900045597823fe04c227
Luke Na <narukeu@outlook.com>
2023-05-08 15:25:21 +0800

    getinfo 方法优化

--- miniprogram/utils/getinfo.js
+++ miniprogram/utils/getinfo.js
@@ -3,28 +3,27 @@
 
 // 因为每一个页面获取信息的方式大致相同，因此这一部分也可以共用。
 
-var http = require("./http.js")
+const http = require("./http.js");
 
 async function getinfo(now_type, now_id) {
-  console.log("now id is:" + now_id)
+  console.log(`now id is: ${now_id}`);
 
   wx.showLoading({
-    title: '正在加载',
-  })
+    title: "正在加载",
+  });
 
   try {
-    const res = await http.cloudGet("/api/model-query/detail/" + now_type + "/" + now_id);
-    var nowinfo = res.data.data;
+    const res = await http.cloudGet(`/api/model-query/detail/${now_type}/${now_id}`);
     wx.hideLoading();
-    return nowinfo;
+    return res.data.data;
   } catch (error) {
     wx.showToast({
-      title: '接口调用失败',
-      icon: 'error',
-      duration: 2000
-    })
+      title: "接口调用失败",
+      icon: "error",
+      duration: 2000,
+    });
     console.error(error);
   }
 }
 
-module.exports = getinfo
+module.exports = getinfo;
