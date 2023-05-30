5d42d8f4b0cc4f2b28781010037748e17cfb54f2
Luke Na <narukeu@outlook.com>
2023-05-30 10:28:02 +0800

    Prettier formatted.

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,18 +2,16 @@
 // 之前用了 TS 插件，引入模块的时候提示不可用，JS 倒是没问题。
 
 // secret.js 是微信云托管的密钥，因此不开源。去掉密钥的 secret.js 为 secret_public.js。
-var secret = require("./utils/secret.js")
+var secret = require("./utils/secret.js");
 
 App({
   onLaunch: function () {
     wx.cloud.init({
       appid: secret.getAppid(),
       envid: secret.getProdId(),
-    })
+    });
   },
   onShow() {},
 
-  onload() {
-
-  }
+  onload() {},
 });
